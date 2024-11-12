49bb369012406791cc817abc5854121269435afb
vannp <vannp@stud.ntnu.no>
2024-11-12 14:35:40 +0100

    delete subscription_id

--- providers.tf
+++ providers.tf
@@ -5,12 +5,12 @@ terraform {
       version = "4.9.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "rgtfoblig02"
-    storage_account_name = "satfoblig02"
-    container_name       = "sctfoblig2l5kaxf1i"
-    key                  = "oblig2.terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "rgtfoblig02"
+  #   storage_account_name = "satfoblig02"
+  #   container_name       = "sctfoblig2l5kaxf1i"
+  #   key                  = "oblig2.terraform.tfstate"
+  # }
 }
 
 provider "azurerm" {
