c8a0a932296bc48bbb88c4682a854c08acb4e90a
vannp <vannp@stud.ntnu.no>
2024-11-12 14:28:20 +0100

    updated

--- providers.tf
+++ providers.tf
@@ -5,16 +5,16 @@ terraform {
       version = "4.9.0"
     }
   }
-  backend "azurerm" {
-    resource_group_name  = "rgtfoblig02"
-    storage_account_name = "satfoblig02"
-    container_name       = "sctfoblig2l5kaxf1i"
-    key                  = "oblig2.terraform.tfstate"
-  }
+  # backend "azurerm" {
+  #   resource_group_name  = "rgtfoblig02"
+  #   storage_account_name = "satfoblig02"
+  #   container_name       = "sctfoblig2l5kaxf1i"
+  #   key                  = "oblig2.terraform.tfstate"
+  # }
 }
 
 provider "azurerm" {
-  subscription_id = "" # set the subscription ID to use
+  subscription_id = "30ee9279-e76e-409d-8973-00c9792f6bcb" # set the subscription ID to use
   features {
   }
 }
