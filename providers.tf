45229c128ac082cb43a9c25ecd02dc022bf8fd6b
vannp <vannp@stud.ntnu.no>
2024-11-13 20:05:12 +0100

    delete subscription_id to sumit

--- providers.tf
+++ providers.tf
@@ -6,16 +6,16 @@ terraform {
     }
 
   }
-  # backend "azurerm" {
-  #   resource_group_name  = "rgtf-oblig2"
-  #   storage_account_name = "sabetfsxv1d0"
-  #   container_name       = "tfstate"
-  #   key                  = "oblig2.terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "rgtf-oblig2"
+    storage_account_name = "sabetfsxv1d0"
+    container_name       = "tfstate"
+    key                  = "oblig2.terraform.tfstate"
+  }
 }
 
 provider "azurerm" {
-  subscription_id = "30ee9279-e76e-409d-8973-00c9792f6bcb" # set the subscription ID to use
+  subscription_id = "" # set the subscription ID to use
   features {
   }
 }
